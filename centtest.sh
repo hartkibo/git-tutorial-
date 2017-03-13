2df5012fe319533cc657509d3c5666b0b64ce6d3
Dinh Ha Nguyen <ha.nguyen@ekaterina.unbelievable-machine.net>
2017-03-13 16:56:47 +0100

    Revert "Sortierskript CentOS"
    This reverts commit 84bedd4b556ac509f17c160c9ccfdf1872641f24.

--- centtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-function Suche() { printf "%30s %10s %10s\n" Hostname OS-System Version echo "__________________________________________________________________________"
-for i in $(grep -h post /uebungsaufgaben/hostlisten/* | grep -i 'centos;7' ); do
-       echo $(echo $i | awk -F ";" {'printf "%30s %10s %10s\n", $1, $4, $5'}) done
-}
-Suche
